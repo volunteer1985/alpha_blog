8cf6090f167a944f2674cbc52363a6534e74504f
volunteer <ichornobai@yahoo.com>
2023-03-15 22:22:25 +0200

    fix of show, add create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def create
-
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 
   def new
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {mininum: 6, maximum: 100}
-  validates :description, presence: true, length: {mininum: 16, maximum: 256}
+  validates :description, presence: true, length: {mininum: 10, maximum: 256}
 end
