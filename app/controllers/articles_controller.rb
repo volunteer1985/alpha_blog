521cf7d33055eacc375fa568a77f5fafa10bda60
volunteer <ichornobai@yahoo.com>
2023-03-16 23:15:24 +0200

    add redirect to new form if article did not save

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,8 +10,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+
   end
 
   def new
