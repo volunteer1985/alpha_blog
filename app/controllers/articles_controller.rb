b1997f15ddb47e0f28596722d0278d0e01bc5bf3
volunteer <ichornobai@yahoo.com>
2023-03-25 09:45:28 +0200

    Refactor articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,17 @@
 class ArticlesController < ApplicationController
 
+  before_action :article_search, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(whitelist_params)
     if @article.save
     flash[:notice] = "Article was created successfully"
       redirect_to @article
@@ -22,12 +24,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def edit
-    @article = Article.find(params[:id])
+    
   end
+
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(whitelist_params)
       flash[:notice] = "Article updated successfully"
       redirect_to @article
     else
@@ -35,11 +38,21 @@ class ArticlesController < ApplicationController
     
     end
   end
+  
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+        @article.destroy
     flash[:notice] = "Article was deleted successfully"
     redirect_to articles_path
+  end
+
+  private
+
+  def whitelist_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def article_search
+    @article = Article.find(params[:id])
   end
 
 end
